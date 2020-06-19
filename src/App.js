52455449e05d0f5907899d880dc9dcb480f8e01f
alex <andr_2001alex_16@mail.ru>
2020-06-19 22:49:28 +0300

    add input

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { Navbar } from './components/Navbar';
+import { Home } from './pages/Home';
+import {BrowserRouter as Router, Switch,Route} from 'react-router-dom'
+import { About } from './pages/About';
+import { Profile } from './pages/Profile';
+import { Alert } from './components/Alert';
+import { AlertState } from './components/context/alert/AlertState';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <AlertState>
+      <Router>
+    <Navbar/>
+      <div className="container pt-4">
+      <Alert alert={{text: 'Test Alert'}}/>
+    <Switch>
+      <Route  path="/"  exact component={Home} />
+      <Route  path="/about" component={About} />
+      <Route  path="/profile/:name" component={Profile} />
+    </Switch>
     </div>
+    </Router>
+    </AlertState>
   );
 }
 
